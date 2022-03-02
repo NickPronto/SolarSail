6e35c8d4879c88545023b9b8a2ed8caf0dabeb04
NickPronto <100635804+NickPronto@users.noreply.github.com>
2022-03-02 09:46:38 -0800

    Update Solar_Array.ino
    updated ignition switch qualifiers and code.

--- Solar_Array.ino
+++ Solar_Array.ino
@@ -1,7 +1,7 @@
 //constants
 unsigned long currentMillis = millis(); // delay period to lower or raise Linear Actuator fully
 unsigned long previousMillis = 0;
-const long intervalLong = 10000; // delay period in between lowering steps.
+const long intervalLong = 1000; // delay period in between lowering steps.
 const long intervalShort = 5000; // delay period in between lowering steps.
 
 //variables 
@@ -12,13 +12,15 @@ int multiplier = 1.2; // multiplier for LDR differences to confirm panel is full
 //input pins
 int rightLDR;
 int leftLDR;
-int ignitionSwitch;
+int ignitionSwitch = 4;
 
 //output pins
 int laRelayDown = 13; // linear actuator 12v relay to extend
 int laRelayUp = 12; // linear actuator 12v relay to retract 
 int leftSLND = 7; // solenoid control 12v relay left hinge
 int rightSLND = 8; // solenoid control 12v relay right hinge
+
+int ignitionSwitchVal;
 
 
 void setup()
@@ -42,23 +44,23 @@ Serial.begin(9600);
 
 void loop(){
 unsigned long currentMillis = millis(); 
-void read();
+void sensorRead();
 
-Serial.print("Left Analog reading: ");
-Serial.print(leftLDR);
+Serial.println("Left Analog reading: ");
+Serial.println(leftLDR);
 Serial.write( '\r' ); //  Carriage Return
-Serial.print("Right Analog reading: ");
-Serial.print(rightLDR);
+Serial.println("Right Analog reading: ");
+Serial.println(rightLDR);
   Serial.write( '\r' ); //  Carriage Return
   delay(1000);
   
-if(ignitionSwitch <= 0 || leftLDR + rightLDR > lightSense){ //only works when there is ample light and the ignition is off.
+if(ignitionSwitchVal == LOW || leftLDR + rightLDR > lightSense){ //only works when there is ample light and the ignition is off.
 
 if(currentMillis - previousMillis >= intervalShort){ // delay in panel movements to avoid bouncing/jittering
   previousMillis = currentMillis;
   if (multiplier*leftLDR >= rightLDR) // "multiplier" here gives buffer to LDR differences while transitioning over from left to right tracking, avoiding a situation where both hinges are disconnected.
   {
-    Serial.print("Stuck Left High Panel");
+    Serial.println("Stuck Left High Panel");
     sensorRead();
     trackLeftHigh();
     lowerLeftPanel();
@@ -66,7 +68,7 @@ if(currentMillis - previousMillis >= intervalShort){ // delay in panel movements
   }
   else if (leftLDR <= multiplier*rightLDR)
   {
-    Serial.print("Stuck Right High Panel");
+    Serial.println("Stuck Right High Panel");
     sensorRead();
     trackRightHigh();
     lowerRightPanel();
@@ -75,7 +77,8 @@ if(currentMillis - previousMillis >= intervalShort){ // delay in panel movements
 }
 }
   else {
-    Serial.print("Stuck Lay Flat");
+    Serial.println("Stuck Lay Flat");
+    sensorRead();
     layFlat();
   }
 
@@ -88,7 +91,7 @@ void sensorRead()
 {
 rightLDR = analogRead(A4); // right LDR sensor input pin
 leftLDR = analogRead(A5); // left LDR sensor input pin
-ignitionSwitch = analogRead(A3); // ignition or kill switch
+ignitionSwitchVal=digitalRead(ignitionSwitch); // ignition or kill switch
 }
 
 void layFlat() // function to drop the panel to its lowest point and lock both hinge solenoids in place for travel.
