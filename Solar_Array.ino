5a20ec25eb634e39b783d07364e9010cd619b781
NickPronto <100635804+NickPronto@users.noreply.github.com>
2022-03-26 20:04:26 -0700

    Update Solar_Array.ino

--- Solar_Array.ino
+++ Solar_Array.ino
@@ -1,6 +1,6 @@
 enum LinearActuator {up, down, off};
 LinearActuator linearActuatorState;
-enum MagLock {pull, noPull};
+enum MagLock {lock, unlock};
 MagLock leftState;
 MagLock rightState;
 
@@ -50,8 +50,8 @@ void setup() {
 
 void loop() {
   sensorRead();
-  if (ignitionSwitchVal < 3) { // only works if the ignition is off
-    if (leftLightValue + rightLightValue > lightSensitivity) { //only works if there is sunlight
+  if (ignitionSwitchVal < HIGH) { // only works if the ignition is off
+    if (leftLightValue + rightLightValue > lightSensitivity*multiplier) { //only works if there is sunlight
       if (leftLightValue > rightLightValue) {
         trackLeftHigh();
         lowerLeftPanel();
@@ -71,7 +71,7 @@ void loop() {
 
 void sensorRead()
 {
-  ignitionSwitchVal = digitalRead(ignitionSwitchVal); // ignition or kill switch
+  ignitionSwitchVal = digitalRead(ignitionSwitch); // ignition or kill switch
   rightLightValue = analogRead(rightLightSensor); // right LDR sensor input pin
   leftLightValue = analogRead(leftLightSensor); // left LDR sensor input pin
 }
@@ -90,10 +90,10 @@ void printOut()
 
 void magLockSwitch(int leftState, int rightState) {
   switch (leftState) {
-    case pull:
+    case lock:
       digitalWrite(magLockLeft, HIGH);
       break;
-    case noPull:
+    case unlock:
       digitalWrite(magLockLeft, LOW);
       break;
     default:
@@ -101,10 +101,10 @@ void magLockSwitch(int leftState, int rightState) {
       break;
 
       switch (rightState) {
-        case pull:
+        case lock:
           digitalWrite(magLockRight, HIGH);
           break;
-        case noPull:
+        case unlock:
           digitalWrite(magLockRight, LOW);
           break;
         default:
@@ -141,16 +141,16 @@ void LinearActuatorSwitch(int linearActuatorState) {
 void layFlat() // function to drop the panel to its lowest point and lock both hinge solenoids in place for travel.
 {
   LinearActuatorSwitch(down);
-  magLockSwitch(pull, pull);
+  magLockSwitch(lock, lock);
   if (currentMillis - previousMillis > interval && ignitionSwitchVal == LOW) { // turn off parasitic drain from MagLocks on batteries at night when panels are lowered.
     previousMillis = currentMillis;
-    magLockSwitch(noPull, noPull);
+    magLockSwitch(unlock, unlock);
   }
 }
 
 void trackLeftHigh() //function to lift and track if the sun is to the relative left of the panel
 {
-  magLockSwitch(pull, noPull);
+  magLockSwitch(lock, unlock);
   LinearActuatorSwitch(up);
   while (leftLightValue >= rightLightValue) {
     sensorRead();
@@ -159,7 +159,7 @@ void trackLeftHigh() //function to lift and track if the sun is to the relative 
 }
 
 void lowerLeftPanel() {
-  magLockSwitch(pull, pull);
+  magLockSwitch(lock, lock);
   if (leftLightValue < multiplier * rightLightValue) {
     trackRightHigh();
   }
@@ -171,7 +171,7 @@ void lowerLeftPanel() {
 
 void trackRightHigh() // function to lift and track if the sun is to the relative right of the panel
 {
-  magLockSwitch(noPull, pull);
+  magLockSwitch(unlock, lock);
   LinearActuatorSwitch(up);
   while (leftLightValue <= rightLightValue) {
     sensorRead();
@@ -181,7 +181,7 @@ void trackRightHigh() // function to lift and track if the sun is to the relativ
 
 void lowerRightPanel()
 {
-  magLockSwitch(pull, pull);
+  magLockSwitch(lock, lock);
   if (leftLightValue < multiplier * rightLightValue) {
     trackRightHigh();
   }
