8b305ac97b055b499c03de1006b196a6c784e4c6
NickPronto <100635804+NickPronto@users.noreply.github.com>
2022-03-31 12:01:30 -0700

    Update Solar_Array.ino
    added app input code. default is set to automatic.

--- Solar_Array.ino
+++ Solar_Array.ino
@@ -1,3 +1,8 @@
+#include <Wire.h>
+#include <BH1750FVI.h>
+
+BH1750FVI LightSensor;
+
 enum LinearActuator {up, down, off};
 LinearActuator linearActuatorState;
 enum MagLock {lock, unlock};
@@ -5,39 +10,43 @@ MagLock leftState;
 MagLock rightState;
 
 //variables
-int lightSensitivity = 10; // adjustable light sensitivity based on ambient light at night
-int multiplier = 1.2; // multiplier for LDR differences to confirm panel is fully down before switching hinge magLocks.
+int lightSensitivity = 100; // adjustable light sensitivity based on ambient light at night
+int manualToggle = 1; //app input for manual mode: 2 "Manual", 1 "Automatic", default automatic, for non-app users.
 
 //input pins
-const int rightLightSensor = A4; // left light sensor
-const int leftLightSensor = A5; //right light sensor
-const int ignitionSwitch = 4; // 12v ignition switch pin, high when vechile ignition is in "run"
-const int windSpeed = 3;
+const int rightLightSensor = 22; // left light sensor
+const int leftLightSensor = 21; //right light sensor
+const int magLockRightSense = 25;
+const int magLockLeftSense = 26;
+const int ignitionSwitch = 34; // 12v ignition switch pin, high when vechile ignition is in "run"
+
 
 //output pins
-const int linearActuatorRelay1 = 9; // linear actuator 12v relay to extend
-const int linearActuatorRelay2 = 10; // linear actuator 12v relay to retract
-const int magLockLeft = 7; // solenoid control 12v relay left hinge
-const int magLockRight = 8; // solenoid control 12v relay right hinge
+const int linearActuatorRelay1 = 4; // linear actuator 12v relay to extend
+const int linearActuatorRelay2 = 2; // linear actuator 12v relay to retract
+const int magLockLeft = 16; // solenoid control 12v relay left hinge
+const int magLockRight = 17; // solenoid control 12v relay right hinge
 
 // Values
-int rightLightValue;
-int leftLightValue;
+uint16_t leftLightValue;
+uint16_t rightLightValue;
+int panelMove;
+
 int ignitionSwitchVal;
 unsigned long currentMillis = millis();
 int previousMillis;
 long interval = 10; // Minutes to wait till turning off parasitic MagLocks off at night. Vehicle must be off to disengage.
+//long delayInterval = 1; //minutes to wait till checking/changing angle
 
 
 void setup() {
   attachInterrupt(digitalPinToInterrupt(ignitionSwitch), layFlat, RISING);
-  Serial.begin(9600);
-  // put your setup code here, to run once:
-  pinMode(10, INPUT_PULLUP);
-  pinMode(9, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
+  LightSensor.begin();
+  LightSensor.SetMode(OneTime_L_resolution_Mode);
+  Serial.begin(115200);
+
+  pinMode(magLockRightSense, INPUT_PULLUP);
+  pinMode(magLockLeftSense, INPUT_PULLUP);
   pinMode(linearActuatorRelay1, OUTPUT);
   pinMode(linearActuatorRelay2, OUTPUT);
   pinMode(magLockLeft, OUTPUT);
@@ -45,13 +54,13 @@ void setup() {
   pinMode(leftLightSensor, INPUT);
   pinMode(rightLightSensor, INPUT);
   pinMode(ignitionSwitch, INPUT);
-  pinMode(windSpeed, INPUT);
+
 }
 
 void loop() {
   sensorRead();
   if (ignitionSwitchVal < HIGH) { // only works if the ignition is off
-    if (leftLightValue + rightLightValue > lightSensitivity*multiplier) { //only works if there is sunlight
+    if (leftLightValue + rightLightValue > lightSensitivity) { //only works if there is sunlight
       if (leftLightValue > rightLightValue) {
         trackLeftHigh();
         lowerLeftPanel();
@@ -68,21 +77,58 @@ void loop() {
 }
 
 //-------Functions below-----------
+void layFlat() // function to drop the panel to its lowest point and lock both hinge solenoids in place for travel.
+{
+  LinearActuatorSwitch(down);
+  magLockSwitch(lock, lock);
+  if (currentMillis - previousMillis > (interval * 60000) && ignitionSwitchVal == LOW) { // turn off parasitic drain from MagLocks on batteries at night when panels are lowered.
+    previousMillis = currentMillis;
+    magLockSwitch(unlock, unlock);
+  }
+}
+
+
+void manualMode() {
+  switch (panelMove) {
+    case 1:
+      leftLightValue = 1 + lightSensitivity;
+      rightLightValue = 0;
+      break;
+    case 2:
+      rightLightValue = 1 + lightSensitivity;
+      leftLightValue = 0;
+      break;
+    case 3:
+      rightLightValue = leftLightValue;
+      break;
+    case 4:
+      layFlat();
+      break;
+    default:
+      layFlat();
+  }
+}
 
 void sensorRead()
 {
   ignitionSwitchVal = digitalRead(ignitionSwitch); // ignition or kill switch
-  rightLightValue = analogRead(rightLightSensor); // right LDR sensor input pin
-  leftLightValue = analogRead(leftLightSensor); // left LDR sensor input pin
+  if (manualToggle = 2) {
+    manualMode();
+  }
+  else {
+    LightSensor.SetAddress(Device_Address_H);
+    leftLightValue = LightSensor.GetLightIntensity();// Get Lux value left
+    LightSensor.SetAddress(Device_Address_L);
+    rightLightValue = LightSensor.GetLightIntensity();// Get Lux value right
+  }
 }
-
 void printOut()
 {
   Serial.print("Ignition on: ");
   Serial.println(ignitionSwitchVal);
-  Serial.print("Left Analog reading: ");
+  Serial.print("Left light sensor reading: ");
   Serial.println(leftLightValue);
-  Serial.print("Right Analog reading: ");
+  Serial.print("Right light sensor reading: ");
   Serial.println(rightLightValue);
   Serial.println();
   Serial.println();
@@ -138,16 +184,6 @@ void LinearActuatorSwitch(int linearActuatorState) {
   }
 }
 
-void layFlat() // function to drop the panel to its lowest point and lock both hinge solenoids in place for travel.
-{
-  LinearActuatorSwitch(down);
-  magLockSwitch(lock, lock);
-  if (currentMillis - previousMillis > (interval*60000) && ignitionSwitchVal == LOW) { // turn off parasitic drain from MagLocks on batteries at night when panels are lowered.
-    previousMillis = currentMillis;
-    magLockSwitch(unlock, unlock);
-  }
-}
-
 void trackLeftHigh() //function to lift and track if the sun is to the relative left of the panel
 {
   magLockSwitch(lock, unlock);
@@ -160,7 +196,7 @@ void trackLeftHigh() //function to lift and track if the sun is to the relative 
 
 void lowerLeftPanel() {
   magLockSwitch(lock, lock);
-  if (leftLightValue < multiplier * rightLightValue) {
+  if (magLockRightSense == HIGH) {
     trackRightHigh();
   }
   while (leftLightValue <= rightLightValue) {
@@ -182,8 +218,8 @@ void trackRightHigh() // function to lift and track if the sun is to the relativ
 void lowerRightPanel()
 {
   magLockSwitch(lock, lock);
-  if (leftLightValue < multiplier * rightLightValue) {
-    trackRightHigh();
+  if (magLockLeftSense == HIGH) {
+    trackLeftHigh();
   }
   while (leftLightValue <= rightLightValue) {
     LinearActuatorSwitch(down);
