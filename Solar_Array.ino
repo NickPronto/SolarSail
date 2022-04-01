e0e1b3832f19e166faac171bda11e5a2c357b22c
NickPronto <100635804+NickPronto@users.noreply.github.com>
2022-04-01 14:45:57 -0700

    Update Solar_Array.ino
    Shifted delay timer out of sensor read function to allow correct order of operations for lowering/lifting to occur.

--- Solar_Array.ino
+++ Solar_Array.ino
@@ -63,65 +63,65 @@ void setup() {
 
 void loop() {
   sensorRead();
-  if (leftLightValue > rightLightValue) {
-    trackLeft();
-  }
-  else {
-    trackRight();
-  }
-}
-
-//-------Functions below-----------
-void layFlat() // function to drop the panel to its lowest point and lock both hinge solenoids in place for travel.
-{
-  LinearActuatorSwitch(down);
-  magLockSwitch(lock, lock);
-  if (currentMillis - previousMillis > (interval * 60000) && ignitionSwitchVal == LOW) { // turn off parasitic drain from MagLocks on batteries at night when panels are lowered.
-    magLockSwitch(unlock, unlock);
-    previousMillis = currentMillis;
-  }
-}
-
-
-void manualSwitch(int) {
-  switch (panelMove) {
-    case left:
-      leftLightValue = 1 + lightSensitivity;
-      rightLightValue = 0;
-      break;
-    case right:
-      rightLightValue = 1 + lightSensitivity;
-      leftLightValue = 0;
-      break;
-    case wait:
-      rightLightValue = leftLightValue;
-      break;
-    case flat:
-      layFlat();
-      break;
-    default:
-      rightLightValue = leftLightValue;
-      break;
-  }
-}
-
-void sensorRead()
-{
-  magLockRightSense = digitalRead(magLockRightSense_pin);
-  magLockLeftSense = digitalRead(magLockLeftSense_pin);
-  ignitionSwitchVal = digitalRead(ignitionSwitch_pin); // ignition or kill switch
-  if (ignitionSwitchVal == LOW) { // only works if the ignition is off
-    if (manualToggle = 2) { //toggle is switched to manual mode.
-      manualSwitch(wait); // waiting on user to press a button. any button.
+  if (currentMillis - previousMillis > delayInterval * 60000) {
+    if (leftLightValue > rightLightValue) {
+      trackLeft();
     }
     else {
-      if (leftLightValue + rightLightValue > lightSensitivity) { //only works if there is sunlight
-        if (currentMillis - previousMillis > delayInterval * 60000) {
+      trackRight();
+    }
+    currentMillis = previousMillis;
+  }
+
+  //-------Functions below-----------
+  void layFlat() // function to drop the panel to its lowest point and lock both hinge solenoids in place for travel.
+  {
+    LinearActuatorSwitch(down);
+    magLockSwitch(lock, lock);
+    if (currentMillis - previousMillis > (interval * 60000) && ignitionSwitchVal == LOW) { // turn off parasitic drain from MagLocks on batteries at night when panels are lowered.
+      magLockSwitch(unlock, unlock);
+      previousMillis = currentMillis;
+    }
+  }
+
+
+  void manualSwitch(int) {
+    switch (panelMove) {
+      case left:
+        leftLightValue = 1 + lightSensitivity;
+        rightLightValue = 0;
+        break;
+      case right:
+        rightLightValue = 1 + lightSensitivity;
+        leftLightValue = 0;
+        break;
+      case wait:
+        rightLightValue = leftLightValue;
+        break;
+      case flat:
+        layFlat();
+        break;
+      default:
+        rightLightValue = leftLightValue;
+        break;
+    }
+  }
+
+  void sensorRead()
+  {
+    magLockRightSense = digitalRead(magLockRightSense_pin);
+    magLockLeftSense = digitalRead(magLockLeftSense_pin);
+    ignitionSwitchVal = digitalRead(ignitionSwitch_pin); // ignition or kill switch
+    if (ignitionSwitchVal == LOW) { // only works if the ignition is off
+      if (manualToggle = 2) { //toggle is switched to manual mode.
+        manualSwitch(wait); // waiting on user to press a button. any button.
+      }
+      else {
+        if (leftLightValue + rightLightValue > lightSensitivity) { //only works if there is sunlight
           LightSensor.SetAddress(Device_Address_H);
           leftLightValue = LightSensor.GetLightIntensity();// Get Lux value left
           LightSensor.SetAddress(Device_Address_L);
           rightLightValue = LightSensor.GetLightIntensity();// Get Lux value right
-          currentMillis = previousMillis;
         }
       }
       else {
