6daa034afe2540151fda51544a51e5e6ec908fe4
Matteo Cordray <matteo.cordray@gmail.com>
2022-04-05 10:03:05 -0400

    add bluetooth template

---
+++ SolarSailApp/solar_sail/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_ble_lib/FlutterBleLibPlugin.h>)
+#import <flutter_ble_lib/FlutterBleLibPlugin.h>
+#else
+@import flutter_ble_lib;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterBleLibPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBleLibPlugin"]];
+}
+
+@end
